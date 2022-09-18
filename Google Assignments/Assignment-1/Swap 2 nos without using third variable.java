6ad0c2e778bcb8bd9cf21cffeca312b46b2a3c0d
JAGAPREETHA-B <108733892+JAGAPREETHA-B@users.noreply.github.com>
2022-09-18 14:23:36 +0530

    Update Swap 2 nos without using third variable.java

--- Google Assignments/Assignment-1/Swap 2 nos without using third variable.java
+++ Google Assignments/Assignment-1/Swap 2 nos without using third variable.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 class Main{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();//10
-    int b=sc.nextInt();//20
-    a=a+b;//30
-    b=a-b;//30-20=10
-    a=a-b;//30-10=20
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    a=a+b;
+    b=a-b;
+    a=a-b;
     System.out.println(a+" "+b);
   }
 }
