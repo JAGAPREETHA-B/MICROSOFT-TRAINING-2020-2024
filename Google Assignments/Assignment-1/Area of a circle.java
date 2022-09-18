0169fc24b40c815f6d1eb3692d0fd9bec4b7a011
JAGAPREETHA-B <108733892+JAGAPREETHA-B@users.noreply.github.com>
2022-09-18 10:34:18 +0530

    Update Area of a circle.java

--- Google Assignments/Assignment-1/Area of a circle.java
+++ Google Assignments/Assignment-1/Area of a circle.java
@@ -1,1 +1,10 @@
 
+import java.util.Scanner;
+class Main{
+   public static void main(String args[]) {   
+      Scanner sc= new Scanner(System.in);
+      int rad= sc.nextInt();
+      float area=(22*rad*rad)/7 ;
+      System.out.println("Area of Circle is: "+area);      
+   }
+}
