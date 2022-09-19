b6cd5d1104eb97e1a9d7fb039b92f046fce6bc69
JAGAPREETHA-B <108733892+JAGAPREETHA-B@users.noreply.github.com>
2022-09-19 23:35:12 +0530

    Create Binary Tree Postorder Traversal.java

---
+++ MICROSOFT/In-Class Assignments/Week-4/Binary Tree Postorder Traversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    List<Integer> list=new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root==null){
+            return list;
+        }
+        else{
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            list.add(root.val);
+        }
+        return list;
+    }
+}
